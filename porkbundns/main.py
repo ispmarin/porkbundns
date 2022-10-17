8e3e5a7ecce5d0430a34525e9fe7460254284dfa
Ivan Marin <ivan@tunguska.cc>
2022-10-17 10:39:31 -0300

    feat: add logging and cli arguments

--- porkbundns/main.py
+++ porkbundns/main.py
@@ -1,8 +1,32 @@
+#!/usr/bin/env python3
+import sys
+import argparse
 import logging
 
 from dns import manage_records
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    manage_records.bulk_update("data/homelab.csv", ".env/base.json")
+
+    parser = argparse.ArgumentParser(description="Update DNS records on Porkbun")
+    parser.add_argument(
+        "-f",
+        "--file_host",
+        type=str,
+        dest="host_file",
+        help="File with host DNS definitions, CSV format",
+    )
+    parser.add_argument(
+        "-d",
+        "--definitions",
+        type=str,
+        dest="def_file",
+        default=".env/base.json",
+        help="File with Porkbun credentials, JSON format",
+    )
+    args = parser.parse_args(args=None if sys.argv[1:] else ["--help"])
+
+    logger.info("Starting DNS update using Porkbun API")
+    manage_records.bulk_update(args.host_file, args.def_file)
